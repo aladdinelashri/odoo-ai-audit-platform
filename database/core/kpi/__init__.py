d4027745032cd9823b08ad748aafd2645a8b95af
aladdinelashri <aladdin.elashri@gmail.com>
2026-07-18 04:48:46 +0300

    Add POS category comparison KPI

--- database/core/kpi/__init__.py
+++ database/core/kpi/__init__.py
@@ -1,3 +1,4 @@
 from .kpi_engine import KPIEngine
 from .pos_kpi_calculator import POSKPICalculator
 from .branch_comparison import BranchComparison
+from .category_comparison import CategoryComparison
