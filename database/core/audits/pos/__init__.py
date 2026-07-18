fabed4e96e18644b6e270ab7c4d05f985c83a201
aladdinelashri <aladdin.elashri@gmail.com>
2026-07-18 06:15:17 +0300

    Add POS discount audit

--- database/core/audits/pos/__init__.py
+++ database/core/audits/pos/__init__.py
@@ -3,3 +3,4 @@ from .refund_audit import RefundAudit
 from .payment_audit import PaymentAudit
 from .session_audit import SessionAudit
 from .cash_audit import CashAudit
+from .discount_audit import DiscountAudit
