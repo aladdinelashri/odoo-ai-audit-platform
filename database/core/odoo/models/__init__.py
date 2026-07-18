a575583d37719769d253fa99b9ead6a5a5eceba0
aladdinelashri <aladdin.elashri@gmail.com>
2026-07-18 15:46:43 +0300

    Add Odoo model registry foundation

---
+++ database/core/odoo/models/__init__.py
@@ -0,0 +1,3 @@
+from .pos_models import POSModels
+from .accounting_models import AccountingModels
+from .model_registry import OdooModelRegistry
---
+++ database/core/odoo/models/accounting_models.py
@@ -0,0 +1,10 @@
+class AccountingModels:
+
+    def list_models(self):
+        return [
+            "account.move",
+            "account.move.line",
+            "account.payment",
+            "account.journal",
+            "account.tax",
+        ]
