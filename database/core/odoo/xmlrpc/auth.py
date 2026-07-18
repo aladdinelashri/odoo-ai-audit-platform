86484f8ddb9c4e65206b8a6b79848ec6f6bacec5
aladdinelashri <aladdin.elashri@gmail.com>
2026-07-18 20:39:09 +0300

    Add XML-RPC authentication foundation

--- database/core/odoo/xmlrpc/auth.py
+++ database/core/odoo/xmlrpc/auth.py
@@ -1,2 +1,18 @@
+import xmlrpc.client
+
+
 class XMLRPCAuth:
-    pass
+    def __init__(self, url, db, username, password):
+        self.url = url
+        self.db = db
+        self.username = username
+        self.password = password
+
+    def authenticate(self):
+        common = xmlrpc.client.ServerProxy(f"{self.url}/xmlrpc/2/common")
+        return common.authenticate(
+            self.db,
+            self.username,
+            self.password,
+            {}
+        )
--- database/core/odoo/xmlrpc/object_service.py
+++ database/core/odoo/xmlrpc/object_service.py
@@ -1,2 +1,10 @@
+import xmlrpc.client
+
+
 class XMLRPCObjectService:
-    pass
+
+    def __init__(self, url, db, uid, password):
+        self.models = xmlrpc.client.ServerProxy(f"{url}/xmlrpc/2/object")
+        self.db = db
+        self.uid = uid
+        self.password = password
