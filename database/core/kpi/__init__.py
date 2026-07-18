805ad1f0786a73f5ef24920523670be53e177b96
aladdinelashri <aladdin.elashri@gmail.com>
2026-07-18 04:57:20 +0300

    Add POS user performance KPI

--- database/core/kpi/__init__.py
+++ database/core/kpi/__init__.py
@@ -3,3 +3,4 @@ from .pos_kpi_calculator import POSKPICalculator
 from .branch_comparison import BranchComparison
 from .category_comparison import CategoryComparison
 from .product_performance import ProductPerformance
+from .user_performance import UserPerformance
