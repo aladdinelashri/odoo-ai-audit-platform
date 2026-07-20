73218520701539e357552594fa2ad18177fbe245
aladdinelashri <aladdin.elashri@gmail.com>
2026-07-20 10:04:42 +0300

    Add ProductTemplateReader

--- database/core/odoo/readers/__init__.py
+++ database/core/odoo/readers/__init__.py
@@ -2,10 +2,12 @@ from .company_reader import CompanyReader
 from .model_reader import ModelReader
 from .partner_reader import PartnerReader
 from .user_reader import UserReader
+from .product_template_reader import ProductTemplateReader
 
 __all__ = [
     "CompanyReader",
     "ModelReader",
     "PartnerReader",
     "UserReader",
+    "ProductTemplateReader",
 ]
