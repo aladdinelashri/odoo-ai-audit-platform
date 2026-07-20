8bde3f1b4d1efcc08d0817ce4ffab8f195db2dd4
aladdinelashri <aladdin.elashri@gmail.com>
2026-07-20 10:14:49 +0300

    Add POSCategoryReader

--- database/core/odoo/readers/__init__.py
+++ database/core/odoo/readers/__init__.py
@@ -4,6 +4,7 @@ from .partner_reader import PartnerReader
 from .user_reader import UserReader
 from .product_template_reader import ProductTemplateReader
 from .product_product_reader import ProductProductReader
+from .pos_category_reader import POSCategoryReader
 
 __all__ = [
     "CompanyReader",
@@ -12,4 +13,5 @@ __all__ = [
     "UserReader",
     "ProductTemplateReader",
     "ProductProductReader",
+    "POSCategoryReader",
 ]
