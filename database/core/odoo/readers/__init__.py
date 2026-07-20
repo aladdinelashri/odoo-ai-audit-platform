291e23157d49909a4aae2ee0ba99a9f3bb104f7b
aladdinelashri <aladdin.elashri@gmail.com>
2026-07-20 10:00:08 +0300

    Add UserReader

--- database/core/odoo/readers/__init__.py
+++ database/core/odoo/readers/__init__.py
@@ -1,9 +1,11 @@
 from .company_reader import CompanyReader
 from .model_reader import ModelReader
 from .partner_reader import PartnerReader
+from .user_reader import UserReader
 
 __all__ = [
     "CompanyReader",
     "ModelReader",
     "PartnerReader",
+    "UserReader",
 ]
